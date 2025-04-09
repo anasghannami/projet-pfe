9d9b0da8c20e5612f77697022a6663d733f00435
Anass Ghannami <anassghamam60@gmail.com>
2025-04-09 12:20:37 +0100

    Front End (first commit )

---
+++ contact/test3.php
@@ -0,0 +1,20 @@
+<?php
+echo "anass ghannami <br>";
+echo "salut";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>premier page php</title>
+    <link rel="stylesheet" href="../principale/index.html">
+</head>
+
+<body>
+
+</body>
+
+</html>
