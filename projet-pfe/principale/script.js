42146cb1f812fc87d096d977fe553a9fc0b2adaa
Anass Ghannami <anassghamam60@gmail.com>
2025-06-27 14:44:26 +0100

    dernier commit

---
+++ projet-pfe/principale/script.js
@@ -0,0 +1,12 @@
+// Header Scroll 
+let nav = document.querySelector(".navbar");
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 50) {
+        nav.classList.add("header-scrolled");
+    } else {
+        nav.classList.remove("header-scrolled");
+    }
+}
+
+
+// nav hide  
